77a1dc06a3f4a67707549c200d57581dd476e4bd
Galen Sho Sato <galen.sato@gmail.com>
2023-02-07 16:29:05 -0500

    Completed deliverables

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,25 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Header from "./Header";
 import ListingsContainer from "./ListingsContainer";
 
 function App() {
+  const [listings, setListings] = useState([])
+  const [search, setSearch] = useState("")
+
+  useEffect(() => {
+    fetch('http://localhost:6001/listings')
+    .then(resp => resp.json())
+    .then(data => setListings(data))
+  }, [])
+
+  const filteredListings = listings.filter(listing => {
+    return listing.description.toLowerCase().includes(search.toLowerCase())
+  })
+
   return (
     <div className="app">
-      <Header />
-      <ListingsContainer />
+      <Header search={search} setSearch={setSearch}/>
+      <ListingsContainer listings={filteredListings} setListings={setListings}/>
     </div>
   );
 }
